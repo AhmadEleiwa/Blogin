3724b1811a387a3374d54540e1947d6d1fecdc08
ahmadeleiwa <ahmadilawa@gmail.com>
2024-03-17 00:19:35 +0200

    Update the header's links colors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
-                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                <nav class="space-x-4 text-gray-200 hover:text-gray-100 text-sm sm:text-base">
                     @guest
                     <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
                     @if (Route::has('register'))
