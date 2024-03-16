b424e3b50315a91047f9b986da57af8a401cb1aa
ahmadeleiwa <ahmadilawa@gmail.com>
2024-03-17 00:13:03 +0200

    Adding post container cart

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,10 +9,17 @@
 </div>
 
 
-<div class="container">
-    <div><img src="https://picsum.photos/id/239/960/620" alt=""></div>
-    <div>
-        <h2>How to be an expert in 2024</h2>
+<div class="container sm:grid grid-cols-2 gap-15 mx-auto py-15">
+    <div class="mx-2 md:mx-0"><img class="sm:rounded-lg" src="https://picsum.photos/id/239/960/620" alt=""></div>
+    <div class="flex flex-col items-start justify-center m-10 sm:m-0">
+        <h2 class="font-bold text-4xl text-gray-700  uppercase my-5">How to be an expert in 2024</h2>
+        <p class="font-bold text-gray-600 pt-2 text-xl">You can find more information about how to be an expert in 2024
+        </p>
+        <p class="py-4 text-gray-500 text-sm leading-5">Contented get distrusts certainty nay are frankness concealed
+            ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects. End
+            sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte
+            sympathize was.</p>
+        <a href="/" class="bg-gray-700 text-gray-100 px-5 py-4 uppercase rounded place-self-start">Read more</a>
     </div>
 </div>
 @endsection
