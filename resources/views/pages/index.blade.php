a08e605e0e2151db6a72525c8d817c0f2dee9130
ahmadeleiwa <ahmadilawa@gmail.com>
2024-03-17 14:55:02 +0200

    Add Recent Post Section

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -28,14 +28,58 @@
 <div class="text-center p-15 bg-green-500 text-gray-100">
     <h2 class="text-2xl">Blog Categories</h2>
     <div class="container mx-auto pt-10 sm:grid grid-cols-4">
-        <span class="font-bold text-2xl py-3">UX Desgin</span>
-        <span class="font-bold text-2xl py-3">Programming</span>
-        <span class="font-bold text-2xl py-3">Graphic Design</span>
-        <span class="font-bold text-2xl py-3">Science</span>
-        <span class="font-bold text-2xl py-3">Engineering</span>
-        <span class="font-bold text-2xl py-3">Art</span>
-        <span class="font-bold text-2xl py-3">Business</span>
-        <span class="font-bold text-2xl py-3">Photography</span>
+        <span class="font-bold text-xl py-3">UX Desgin</span>
+        <span class="font-bold text-xl py-3">Programming</span>
+        <span class="font-bold text-xl py-3">Graphic Design</span>
+        <span class="font-bold text-xl py-3">Science</span>
+        <span class="font-bold text-xl py-3">Engineering</span>
+        <span class="font-bold text-xl py-3">Art</span>
+        <span class="font-bold text-xl py-3">Business</span>
+        <span class="font-bold text-xl py-3">Photography</span>
+    </div>
+</div>
+
+
+{{-- Recent Posts --}}
+
+<div class="container mx-auto text-center py-15">
+    <h2 class="text-5xl fonty-bold py-10">Recent Post</h2>
+    <p class="text-gray-400 leading-5 px-10">Contented get distrusts certainty nay are frankness concealed Contented get
+        distrusts certainty nay are frankness concealed Contented get distrusts certainty nay are frankness concealed
+        Contented get distrusts certainty nay are frankness concealed</p>
+
+</div>
+<div class="sm:grid grid-cols-2 mx-auto w-4/5">
+    <div class="flex bg-gray-700 text-gray-100 pt-10">
+        <div class="block m-auto pt-4 pb-15 w-4/5">
+            <ul class="md:flex text-xs gap-2">
+                <li
+                    class="bg-gray-100 text-gray-700 p-2 rounded inline-block my-1 md:my-0 hover:bg-gray-500 hover:text-gray-100 transition-all duration-100">
+                    <a href="">PHP</a>
+                </li>
+                <li
+                    class="bg-gray-100 text-gray-700 p-2 rounded inline-block my-1 md:my-0 hover:bg-gray-500 hover:text-gray-100 transition-all duration-100">
+                    <a href="">Langauges</a>
+                </li>
+                <li
+                    class="bg-gray-100 text-gray-700 p-2 rounded inline-block my-1 md:my-0 hover:bg-gray-500 hover:text-gray-100 transition-all duration-100">
+                    <a href="">Backend</a>
+                </li>
+            </ul>
+            <h3 class=" text-base py-10 leading-6">
+                Contented get distrusts certainty nay are frankness concealed
+                ham. On unaffected resolution on considered of. No thought me husband or colonel forming effects.
+                End
+                sitting shewing who saw besides son musical adapted. Contrasted interested eat alteration pianoforte
+            </h3>
+
+            <a href="/"
+                class="bg-transparent text-sm border-2 font-bold text-gray-100 py-4 px-5 uppercase rounded-lg inline-block my-1 md:my-0 hover:bg-gray-500 hover:text-gray-100 transition-all duration-100">Read
+                more</a>
+        </div>
+    </div>
+    <div class="flex">
+        <img class="obect-cover" src="https://picsum.photos/id/242/960/620" alt="">
     </div>
 </div>
 @endsection
