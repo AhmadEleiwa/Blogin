d8cf37030e40d8cae36110a22c22990ca001d918
ahmadeleiwa <ahmadilawa@gmail.com>
2024-03-22 00:19:06 +0200

    Update the posts to be taken from the database

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -7,44 +7,25 @@
     <h1 class="text-6xl font-bold">All posts</h1>
 </div>
 
+@foreach ( $posts as $post )
+    
 
 
 <div class="container mx-auto sm:grid grid-cols-2 gap-15 py-15 px-5 border-b border-gray-300 ">
     <div class="flex">
-        <img class="object-cover" src="https://picsum.photos/id/343/960/620" alt="">
+        <img class="object-cover" src="{{$post->image_path}}" alt="">
     </div>
     <div>
-        <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">How To Create Laravel Blog</h2>
+        <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">{{$post->title}}</h2>
         <div>
-            By: <span class="text-gray-500 italic">Ahmad</span>
-            <p class=" text-lg text-gray-700 py-8 leading-6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ad
-                ipsa doloribus, inventore ut velit minus quidem
-                ea tempore vitae voluptates eligendi fugit maiores enim eaque distinctio eveniet error quia quo?</p>
-            <a href="/"
+            By: <span class="text-gray-500 italic">{{$post->user->name}}</span>
+            <p class=" text-lg text-gray-700 py-8 leading-6 ">{{$post->description}}</p>
+            <a href="/blog/{{$post->slug}}"
                 class="bg-gray-700 text-xs text-gray-100 px-5 py-4 uppercase rounded place-self-start hover:bg-gray-500 transistion-all duration-100">Read
                 more</a>
         </div>
     </div>
 </div>
-
-<div class="container mx-auto sm:grid grid-cols-2 gap-15 py-15 px-5 border-b border-gray-300 ">
-    <div class="flex">
-        <img class="object-cover" src="https://picsum.photos/id/343/960/620" alt="">
-    </div>
-    <div>
-        <h2 class="text-gray-700 font-bold text-4xl py-5 md:pt-0">How To Create Laravel Blog</h2>
-        <div>
-            By: <span class="text-gray-500 italic">Ahmad</span>
-            <p class=" text-lg text-gray-700 py-8 leading-6 ">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Ad
-                ipsa doloribus, inventore ut velit minus quidem
-                ea tempore vitae voluptates eligendi fugit maiores enim eaque distinctio eveniet error quia quo?</p>
-            <a href="/"
-                class="bg-gray-700 text-xs text-gray-100 px-5 py-4 uppercase rounded place-self-start hover:bg-gray-500 transistion-all duration-100">Read
-                more</a>
-        </div>
-    </div>
-</div>
+@endforeach
 
 @endsection
